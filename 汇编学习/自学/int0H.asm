2dfdb34681b89b8ce9571b2602e83a904cdb0479
suyucheng-lz <suyuchenglz@163.com>
2020-02-10 17:36:56 +0800

    a new file

---
+++ 汇编学习/自学/int0H.asm
@@ -0,0 +1,36 @@
+assume cs:code
+data segment
+        db "Hello World!"
+data ends
+code segment
+start:
+        mov ax,cs 
+        mov ds,ax
+        mov si,offser dp0   ;设置ds:si指向源地址
+        mov ax,0
+        mov es,ax
+        mov di,200h         ;设置es:di指向目的地址
+        mov cx,offset do0end- offset do0    ;设置cx为传输长度
+        cld                 ;设置传输方向为正
+        rep movsb
+
+        mov ax,0;设置中断向量表
+        mov es,ax
+        mov word ptr es:[0*4],200h
+        mov word ptr es:[0*4+2],0
+
+        mov ax,4c00h
+        int 21h
+do0:    ;显示字符串
+        mov ax,data 
+        mov ds,ax
+        mov si,0            ;设置ds:si指向字符串
+        mov ax,0b800h
+        mov es,ax
+        mov di,12*160+36*2  ;设置es:di指向显存空间的中间位置
+        mov cx,21           ;设置cx为字符串长度
+doend:
+        nop
+code ends
+end start
+;"-"是编译器可以识别的运算符号，两个常数的减法，编译器还可以处理很多表达式
